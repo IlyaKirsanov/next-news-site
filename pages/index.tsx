ee6851b94140cdbf1990ed80b0dadfab3aa8e07b
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-15 14:32:50 +0300

    create app component with theme provider

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Head from "next/head";
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+export default function Front() {
+  return (
+    <>
+      <Head>
+        <title>Front page of the news application</title>
+      </Head>
+      <main>
+
+      </main>
+    </>
+  );
+}
