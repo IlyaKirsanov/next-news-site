15dbfa010069f2820874af83099f43634dc26c5b
IlyaKirsanov <ilya.kirsanov@gmail.com>
2021-07-21 10:45:28 +0300

    Create API for comments

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,7 @@ import { Post } from "../shared/types";
 
 const categories = require("./categories.json");
 const posts = require("./posts.json");
+const comments = require("./comments.json");
 const app = express();
 
 app.use(cors());
@@ -19,6 +20,9 @@ app.get("/posts", (_, res) => {
 app.get("/posts/:id", (req, res) => {
   const wantedId = String(req.params.id);
   const post = posts.find(({ id }: Post) => String(id) === wantedId);
+  console.log(`==========================================`);
+  console.log(`GET SINGLE POST SERVER ${wantedId}`);
+  console.log(`==========================================`);
   return res.json(post);
 });
 
@@ -30,9 +34,23 @@ app.get("/categories/:id", (req, res) => {
   const { id } = req.params;
   const found = posts.filter(({ category }: Post) => category === id);
   const categoryPosts = [...found, ...found, ...found];
+  console.log(`==========================================`);
+  console.log(`GET CATEGORIES FROM SERVER ${found}`);
+  console.log(`==========================================`);
   return res.json(categoryPosts);
 });
 
-app.listen(port, () =>
-  console.log(`DB is running on http://localhost:${port}!`)
-);
+app.get("/comments/:post", (req, res) => {
+  const postId = Number(req.params.post);
+  const foundComments = comments.filter((comment) => comment.post === postId);
+  console.log(`==========================================`);
+  console.log(`GET COMMENTS FOR POST ${postId}`);
+  console.log(`==========================================`);
+  return res.json(foundComments);
+});
+
+app.listen(port, () => {
+  console.log(`==========================================`);
+  console.log(`DB is running on http://localhost:${port}!`);
+  console.log(`==========================================`);
+});
